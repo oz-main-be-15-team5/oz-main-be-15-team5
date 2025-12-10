60c62925079ca04fa058604766f068a72c478c1f
HuiJeong <hwayuya@gmail.com>
2025-12-10 19:16:37 +0900

    **Commit Title Type**
    - ✨ feat : 
    - 🐛 fix : Tortoise ORM 대신 SQLAlchemy 사용으로 코드 일괄 통일
    - 💡 chore : 오타 수정
    - 📝 docs : 
    - 🚚 build :
    - ✅ test : 
    - ♻️ refactor : 
    - 🚑 hotfix :

--- app/models.py
+++ app/models.py
@@ -1,31 +1,29 @@
-from tortoise import fields, models
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import datetime
-from datetime import datetime
 from app.db import Base
 
 
 # Tortoise ORM User 모델 정의
 class User(models.Model):
+
     # user ID
-    id = fields.IntField(pk=True)
+    id = Column(Integer, primary_key=True, index=True)
 
     # 비밀번호 해시 : 암호화된 비밀번호 저장
     # 보안을 위해 평문 비밀번호는 X
-    password_hash = fields.CharField(max_length=255, null=False)
+    password_hash = Column(String(255), nullable=False)
 
     # 유저명 (UNIQUE)
-    username = fields.CharField(max_length=100, unique=True, null=False)
+    username = Column(String(100), unique=True, nullable=False)
 
     # 이메일 (UNIQUE)
-    email = fields.CharField(max_length=100, unique=True, null=False)
+    email = Column(String(100), unique=True, nullable=False)
 
     # 가입일시 (자동생성)
-    created_at = fields.DatetimeField(auto_now_add=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
-    # 실제 데이터베이스의 연결될 테이블명
-    class Meta:
-        table = "users"
+    diaries = relationship("Diary", back_populates="user")
 
     # 파이썬 객체를 문자열로 반환하도록 정의
     def __str__(self):
@@ -35,7 +33,7 @@ class User(models.Model):
 class Diary(Base):
     __tablename__ = "diaries"
     
-    id = Cloumn(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("user.id"))
     title = Column(String(200), nullable=False)
     content = Column(Text, nullable=False)
